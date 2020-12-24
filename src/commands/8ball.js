6fac094597b5811ddfce912e271d4aee028bf815
josuerv99 <josuerojasvega@gmail.com>
2020-12-24 13:25:39 -0600

    <8ball>: cleaning...

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -7,23 +7,11 @@ module.exports = {
     description : 'Let the magic 8 ball decide for your future!',
     help: '<question>',
     execute(message, args) {
-        if (args.length > 0) {
-            const embed = new Discord.MessageEmbed()
-                .setTitle("(8) ball")
-                .setColor(0x00A2A2)
-                .setDescription(`=> ${randomMessage(responses)}`)
-                .setThumbnail("https://i.imgur.com/IIzf5ao.png");
-            message.channel.send(embed);
-        }
-        else 
-        {
-            const embed = new Discord.MessageEmbed()
-                .setTitle("(8) ball")
-                .setColor(0x00A2A2)
-                .setDescription(`====> Ask me about what you want to know about the future `)
-                .setThumbnail("https://i.imgur.com/IIzf5ao.png");
-                
-            message.channel.send(embed);
-        }
+        const embed = new Discord.MessageEmbed()
+            .setTitle("(8) ball")
+            .setColor(0x00A2A2)
+            .setDescription(args.length > 0 ? `=> ${randomMessage(responses)}` : `====> Ask me about what you want to know about the future `)
+            .setThumbnail("https://i.imgur.com/IIzf5ao.png");
+        message.channel.send(embed);  
     } 
 };
