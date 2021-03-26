08c20f6452795569237ec477a0e92aa75b8ccafe
Josue Rojas Vega <31432526+JOSUERV99@users.noreply.github.com>
2021-03-26 16:54:06 -0600

    Batch: batch file to run the discord bot instance from my pc

--- run_cc_bot.bat
+++ run_cc_bot.bat
@@ -7,4 +7,4 @@ Run CodeAndChillBot instance from local (WindowsStart)
 cd "C:\Users\Josue\Documents\git\CodeAndChillBot" && npm start
 
 :add_routine
-schtasks /create /tn "CodeAndChill BOT Routine" /tr C:\Users\Josue\Desktop\run_c\\&c_bot.bat /sc onstart /sd 26/03/2021
+schtasks /create /tn "CodeAndChill BOT Routine" /tr C:\Users\Josue\Desktop\run_cc_bot.bat /sc onstart /sd 26/03/2021
